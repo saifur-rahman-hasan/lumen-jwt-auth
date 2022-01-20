4c5769d611a9a1f98b77a175049c195b150899b3
Saifur Rahman <saifur.dohs@gmail.com>
2022-01-20 21:13:10 +0600

    Initial Commit

---
+++ routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function(){
+    return response()->json([
+        'appName' => "Soluta Dashboard Report Servie",
+        'appVersion' => app()->version()
+    ]);
+});
+
+/**
+ * Auth Routes
+ */
+Route::group([
+
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
